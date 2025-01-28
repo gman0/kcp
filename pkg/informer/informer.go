b8f67a7001e2f00fa372fd08a6ae24a9b83e6602
Robert Vasek <rvasek01@gmail.com>
2025-01-28 22:34:50 +0100

    pkg/informer: added mutatingadmissionpolicies and mutatingadmissionpolicybindings to builtin types
    Added mutatingadmissionpolicies and mutatingadmissionpolicybindings
    from admissionregistration.k8s.io/v1alpha1 into builtin informable
    types.

--- pkg/informer/informer.go
+++ pkg/informer/informer.go
@@ -853,6 +853,8 @@ var builtInInformableTypes map[schema.GroupVersionResource]GVRPartialMetadata = 
 	gvrFor("admissionregistration.k8s.io", "v1", "validatingwebhookconfigurations"):         withGVRPartialMetadata(apiextensionsv1.ClusterScoped, "ValidatingWebhookConfiguration", "validatingwebhookconfiguration"),
 	gvrFor("admissionregistration.k8s.io", "v1alpha1", "validatingadmissionpolicies"):       withGVRPartialMetadata(apiextensionsv1.ClusterScoped, "ValidatingAdmissionPolicy", "validatingadmissionpolicy"),
 	gvrFor("admissionregistration.k8s.io", "v1alpha1", "validatingadmissionpolicybindings"): withGVRPartialMetadata(apiextensionsv1.ClusterScoped, "ValidatingAdmissionPolicyBinding", "validatingadmissionpolicybinding"),
+	gvrFor("admissionregistration.k8s.io", "v1alpha1", "mutatingadmissionpolicies"):         withGVRPartialMetadata(apiextensionsv1.ClusterScoped, "MutatingAdmissionPolicy", "mutatingadmissionpolicies"),
+	gvrFor("admissionregistration.k8s.io", "v1alpha1", "mutatingadmissionpolicybindings"):   withGVRPartialMetadata(apiextensionsv1.ClusterScoped, "MutatingAdmissionPolicyBinding", "mutatingadmissionpolicybinding"),
 	gvrFor("apiextensions.k8s.io", "v1", "customresourcedefinitions"):                       withGVRPartialMetadata(apiextensionsv1.ClusterScoped, "CustomResourceDefinition", "customresourcedefinition"),
 	gvrFor("core.kcp.io", "v1alpha1", "logicalclusters"):                                    withGVRPartialMetadata(apiextensionsv1.ClusterScoped, "LogicalCluster", "logicalcluster"),
 }
