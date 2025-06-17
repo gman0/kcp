b33ce910ec1f2ce041647dd0e603bd2fe9c440d5
Robert Vasek <rvasek01@gmail.com>
2025-06-17 14:27:11 +0200

    Use DynamicRESTMapper with CachedResource reconciler
    On-behalf-of: @SAP robert.vasek@sap.com
    Signed-off-by: Robert Vasek <robert.vasek@clyso.com>

--- pkg/server/controllers.go
+++ pkg/server/controllers.go
@@ -1721,6 +1721,7 @@ func (s *Server) installCacheController(ctx context.Context, config *rest.Config
 		s.KubeClusterClient,
 		s.KubeSharedInformerFactory.Core().V1().Namespaces(),
 		s.KubeSharedInformerFactory.Core().V1().Secrets(),
+		s.DynRESTMapper,
 		s.DiscoveringDynamicSharedInformerFactory,
 		s.CacheKcpSharedInformerFactory,
 		cachedResourceInformer,
