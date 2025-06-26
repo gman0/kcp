339b9d3b14fbc96cfaebd2f2fd14276299c817c3
Robert Vasek <rvasek01@gmail.com>
2025-06-26 18:02:03 +0200

    repl VW: move APIDomainKey building and parsing into separate pkg

---
+++ pkg/virtual/replication/apidomainkey/apidomainkey.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2025 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apidomainkey
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/kcp-dev/logicalcluster/v3"
+
+	dynamiccontext "github.com/kcp-dev/kcp/pkg/virtual/framework/dynamic/context"
+	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+)
+
+func New(shardName genericapirequest.Shard, clusterName logicalcluster.Name, cachedResource string) dynamiccontext.APIDomainKey {
+	return dynamiccontext.APIDomainKey(fmt.Sprintf("%s/%s/%s", shardName, clusterName, cachedResource))
+}
+
+func Parse(key dynamiccontext.APIDomainKey) (shardName genericapirequest.Shard, cachedResourceCluster logicalcluster.Name, cachedResourceName string, err error) {
+	parts := strings.Split(string(key), "/")
+	if len(parts) != 3 {
+		return "", "", "", fmt.Errorf("invalid APIDomainKey %q for replication VW", string(key))
+	}
+
+	shardName, cachedResourceCluster, cachedResourceName = genericapirequest.Shard(parts[0]), logicalcluster.Name(parts[1]), parts[2]
+	return
+}
